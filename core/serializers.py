1e254d2575f1bb1db418d96b63de43fdc43b5398
Mostafa Osman <mostafa.mahoud1528@gmail.com>
2022-10-11 17:10:35 +0200

    lastcommit

--- core/serializers.py
+++ core/serializers.py
@@ -9,3 +9,5 @@ class UserCreateSerializer(BaseUserCreateSerialzier):
 class UserSerializer(BaseUserSerializer):
     class Meta(BaseUserSerializer.Meta):
         fields= ['id','username','email','first_name','last_name']
+
+        
